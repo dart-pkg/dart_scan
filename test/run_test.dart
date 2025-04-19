3a72da99bce6e27477ec40095c0ca65b6183e722
JavaCommons Technologies <javacommons@gmail.com>
2025-04-19 21:05:34 +0900

    .

--- test/run_test.dart
+++ test/run_test.dart
@@ -2,16 +2,21 @@ import 'package:test/test.dart';
 import 'package:sys/sys.dart' as sys;
 import 'package:output/output.dart';
 import 'package:path/path.dart' as path;
-import 'package:dart_scan/dart_scan.dart';
+//import 'package:dart_scan/dart_scan.dart';
 
 void main() {
   group('Run', () {
     test('run1', () {
       String home = sys.getenv('HOME')!;
       echo(home, 'home');
-      String helloDir = path.join(home, 'dart', 'hello');
-      echo(helloDir, 'helloDir');
-      echo(sys.directoryExists(helloDir), 'sys.directoryExists(helloDir)');
+      String helloAppDir = path.join(home, 'dart', 'hello', 'app');
+      echo(helloAppDir, 'helloDir');
+      echo(sys.directoryExists(helloAppDir), 'sys.directoryExists(helloDir)');
+      List<String> files = sys.pathFiles(helloAppDir);
+      //echo(files, 'files');
+      List<String> sources =
+          files.where((x) => sys.pathExtension(x) == '.dart').toList();
+      echo(sources, '$sources');
     });
     test('run2', () {
       dump('this is run2');
