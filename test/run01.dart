8e16801efaa436e933a784f30d676ff42707a464
JavaCommons Technologies <javacommons@gmail.com>
2025-04-19 19:59:31 +0900

    .

--- test/run01.dart
+++ test/run01.dart
@@ -1,8 +1,16 @@
+import 'dart:io' as io;
 import 'package:output/output.dart';
+import 'package:sys/sys.dart' as sys;
+import 'package:path/path.dart' as path;
 import 'package:dart_scan/dart_scan.dart';
 
 void main() {
-  var calc = new Calculator();
-  var result = calc.addOne(123);
-  dump(result, 'result');
+  //var calc = new Calculator();
+  //var result = calc.addOne(123);
+  //dump(result, 'result');
+  String home = sys.getenv('HOME')!;
+  echo(home, 'home');
+  String helloDir = path.join(home, 'dart', 'hello');
+  echo(helloDir, 'helloDir');
+  echo(io.Directory(helloDir).existsSync(), 'io.Directory(helloDir).exists()');
 }
